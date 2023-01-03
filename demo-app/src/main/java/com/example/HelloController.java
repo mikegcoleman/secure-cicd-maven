d9cc981bb56fc373b0cc536114d8a80fed95e461
Mike Coleman <mikegcoleman@google.com>
2023-01-03 19:00:37 +0000

    fixed

--- demo-app/src/main/java/com/example/HelloController.java
+++ demo-app/src/main/java/com/example/HelloController.java
@@ -18,14 +18,8 @@ public class HelloController {
     @GetMapping("/hello") 
     public String hello()
     {
-<<<<<<< HEAD
         // System.out.println("Database URL:" + env.getProperty("spring.datasource.url"));
         // System.out.println("Database URL:" + env.getProperty("spring.datasource.user"));
-=======
-        // System.out.println("Database URL:" + env.getProperty("spring.datasource.url"));
-        // System.out.println("Database URL:" + env.getProperty("spring.datasource.user"));
-        // 
->>>>>>> 7af0ae1eb89f5fd912a78591907cb5d13b1231a3
         
         return String.format("Hello from your %s environment !!! %s", target, System.getProperty("user.home"));
     }
